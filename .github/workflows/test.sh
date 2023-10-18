47b3dce25542dc63ddd8fc9f7859c8191c8864ed
upendraddev <81978371+upendraddev@users.noreply.github.com>
2023-10-18 15:54:53 +0530

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/env bash
+# VERSION 1.0.0
+echo -e "\n===== [$(readlink -f $0)] =====\n" 1>&2
+set -euvx -o pipefail
+shopt -s inherit_errexit
 
+
+Env="tessghc2456"
+TitleMessage="teysav4556"
+DDAPPKEY="svjgcjmd3564"
+
+echo "acmpcaquota = $DDAPPKEY:$TitleMessage" >> ${GITHUB_OUTPUT}
