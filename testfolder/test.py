d1216ba77b79b1f7814b9e87ffe7a6ff232b46d0
upendraddev <81978371+upendraddev@users.noreply.github.com>
2023-10-19 13:17:11 +0530

    Update test.py

--- testfolder/test.py
+++ testfolder/test.py
@@ -3,3 +3,19 @@ import json
 import sys
 
 print (sys.argv[1:]);
+acicvalue = sys.argv[1]
+gsaqvalue = sys.argv[2]
+title = sys.argv[3]
+testvalue =sys.argv[4]
+
+log_data = {
+    "ddsource": "custom",
+    "tags": ["acmpca-service-quota-limits"],
+    "titlemessage": title,
+    "presentquotaused": acicvalue,
+    "message": "This is a test log message for getting acmpca service quota"
+
+
+}
+print("title",title)
+print("Log data", log_data)
