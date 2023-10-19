0e6348a03257a3457cc36bcd069327fda9f95f87
upendraddev <81978371+upendraddev@users.noreply.github.com>
2023-10-19 12:18:44 +0530

    Update test.sh

--- testfolder/test.sh
+++ testfolder/test.sh
@@ -9,4 +9,4 @@ Env="tessghc2456"
 TitleMessage="teysav4556"
 DDAPPKEY="svjgcjmd3564"
 echo "myvalue='MYVALUE'" >> $GITHUB_OUTPUT
-echo "quotavalue= ${DDAPPKEY} " >> $GITHUB_OUTPUT
+echo "quotavalue= ${DDAPPKEY} : ${TitleMessage} " >> $GITHUB_OUTPUT
