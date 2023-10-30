dcd8b67bc12b64c7ea0854e54db0554d723b1ddd
upendraddev <81978371+upendraddev@users.noreply.github.com>
2023-10-30 20:23:21 +0530

    Update get-aws-account-data.sh

--- testfolder/get-aws-account-data.sh
+++ testfolder/get-aws-account-data.sh
@@ -4,4 +4,5 @@ echo -e "\n===== [$(readlink -f $0)] =====\n" 1>&2
 set -euvx -o pipefail
 shopt -s inherit_errexit
 
+aws iam generate-credential-report --output text 
 aws iam get-credential-report --output text --query Content  | base64 -d
