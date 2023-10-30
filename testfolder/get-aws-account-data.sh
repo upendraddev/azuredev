b167811a726d00d179fcaf893c7eb9e863a1d627
upendraddev <81978371+upendraddev@users.noreply.github.com>
2023-10-30 19:47:57 +0530

    Update get-aws-account-data.sh

--- testfolder/get-aws-account-data.sh
+++ testfolder/get-aws-account-data.sh
@@ -4,6 +4,4 @@ echo -e "\n===== [$(readlink -f $0)] =====\n" 1>&2
 set -euvx -o pipefail
 shopt -s inherit_errexit
 
-STAGING_FILE_PATH="./staging-data.txt"
 aws iam generate-credential-report
-(aws iam get-credential-report --query "Content" --output text | base64 -d) >>$STAGING_FILE_PATH
