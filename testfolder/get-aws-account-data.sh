7fdbbcab9024f04b77fa68d53a9d4e617e9777a6
upendraddev <81978371+upendraddev@users.noreply.github.com>
2023-10-30 20:02:39 +0530

    Update get-aws-account-data.sh

--- testfolder/get-aws-account-data.sh
+++ testfolder/get-aws-account-data.sh
@@ -4,4 +4,4 @@ echo -e "\n===== [$(readlink -f $0)] =====\n" 1>&2
 set -euvx -o pipefail
 shopt -s inherit_errexit
 
-aws iam generate-credential-report
+aws iam get-credential-report --query "State" --output text
