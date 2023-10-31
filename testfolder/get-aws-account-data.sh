02957231559417a62438e24dec3e282ae2c57733
upendraddev <81978371+upendraddev@users.noreply.github.com>
2023-10-31 13:31:36 +0530

    Update get-aws-account-data.sh

--- testfolder/get-aws-account-data.sh
+++ testfolder/get-aws-account-data.sh
@@ -7,19 +7,17 @@ shopt -s inherit_errexit
 STAGING_FILE_PATH="./staging-data.txt"
 
 G_STATUS=$(aws iam generate-credential-report --output text)
-G_STATUS1="COMPLETE"
+echo "$G_STATUS"
+# G_STATUS1="COMPLETE"
 # until aws iam generate-credential-report --output text | grep $G_STATUS1; do echo \"Waiting for report generation complete...\"; sleep 3; done; \
-if ["$G_STATUS" != "COMPLETE" ]
-then
+if ["$G_STATUS" != "COMPLETE" ]; then
 sleep 30
 G_STATUS2=$(aws iam generate-credential-report --output text)
-echo $G_STATUS2
- if [ "$G_STATUS2" == "COMPLETE" ]
- then
-   (aws iam get-credential-report --query "Content" --output text | base64 -d) >>$STAGING_FILE_PATH
-   USERS=$(cat $STAGING_FILE_PATH | awk -F "\"*,\"*" 'NR>2 {print $1"|"$7"|"$9"|"$10"|"$14"|"$15}')
-   echo $USERS
-   len=${#USERS[@]}
-   echo $len
-  fi
+echo "$G_STATUS2"
+else
+  (aws iam get-credential-report --query "Content" --output text | base64 -d) >>$STAGING_FILE_PATH
+  USERS=$(cat $STAGING_FILE_PATH | awk -F "\"*,\"*" 'NR>2 {print $1"|"$7"|"$9"|"$10"|"$14"|"$15}')
+  echo $USERS
+  len=${#USERS[@]}
+  echo $len
 fi
