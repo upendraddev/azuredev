161d92a318f7037123756be61c1e358a4444a813
upendraddev <81978371+upendraddev@users.noreply.github.com>
2023-10-31 13:58:42 +0530

    Update get-aws-account-data.sh

--- testfolder/get-aws-account-data.sh
+++ testfolder/get-aws-account-data.sh
@@ -11,7 +11,7 @@ echo "$G_STATUS"
 G_STATUS1="COMPLETE"
 until aws iam generate-credential-report --output text | grep $G_STATUS1; do echo \"Waiting for report generation complete...\"; sleep 10; done; \
 echo "Hello"
-if [[ $G_STATUS -eq "COMPLETE" ]]
+if [ $G_STATUS="COMPLETE" ]
 then
   echo "HEllo1"
 fi
