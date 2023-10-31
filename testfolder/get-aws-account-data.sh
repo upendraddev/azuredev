f5c75292105ba4ec4d3e251ffaef86b7e3569850
upendraddev <81978371+upendraddev@users.noreply.github.com>
2023-10-31 16:00:29 +0530

    Update get-aws-account-data.sh

--- testfolder/get-aws-account-data.sh
+++ testfolder/get-aws-account-data.sh
@@ -11,8 +11,7 @@ G_STATUS=$(aws iam generate-credential-report --output text)
 G_STATUS1="COMPLETE"
 until aws iam generate-credential-report --output text | grep $G_STATUS1; do echo \"Waiting for report generation complete...\"; sleep 30; done; \
 G_STATUS=$(aws iam generate-credential-report --output text)
-if [ $G_STATUS="COMPLETE" ]
-then
+if [ $G_STATUS="COMPLETE" ]; then
   (aws iam get-credential-report --query "Content" --output text | base64 -d) >>$STAGING_FILE_PATH
   USERS=$(cat $STAGING_FILE_PATH | awk -F "\"*,\"*" 'NR>2 {print $1"|"$7"|"$9"|"$10"|"$14"|"$15}')
   echo $USERS
