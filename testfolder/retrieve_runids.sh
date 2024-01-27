2891a80e5e0707f9395073744d5d9928326c6dc4
upendraddev <81978371+upendraddev@users.noreply.github.com>
2024-01-27 20:05:56 +0530

    test

---
+++ testfolder/retrieve_runids.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set the repository owner and name
+REPO_OWNER="upendraddev"
+REPO_NAME="azuredev"
+
+# Set the workflow name
+WORKFLOW_NAME="test-ssla-certs"
+
+# Set the Personal Access Token (PAT)
+GITHUB_TOKEN=${{ secrets.GITHUB_TOKEN }}
+
+# Set the GitHub API URL for workflow runs
+API_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/workflows/${WORKFLOW_NAME}/runs"
+
+# Use curl to retrieve workflow runs
+response=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" $API_URL)
+
+# Extract workflow run IDs using jq (make sure jq is installed)
+workflow_run_ids=$(echo "$response" | jq -r '.workflow_runs[].id')
+
+# Print the workflow run IDs
+echo "Workflow Run IDs:"
+echo "$workflow_run_ids"
