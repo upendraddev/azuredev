9721b412ac38f23d1bbd67ad548e02360f5c7447
upendraddev <81978371+upendraddev@users.noreply.github.com>
2024-01-27 20:44:28 +0530

    test

--- testfolder/retrieve_runids.sh
+++ testfolder/retrieve_runids.sh
@@ -4,49 +4,20 @@
 REPO_OWNER="upendraddev"
 REPO_NAME="azuredev"
 
-# Set the workflow run ID for which you want to retrieve artifacts
-WORKFLOW_RUN_ID="7677580437"
-
-# Set the name of the artifact you want to download
-ARTIFACT_NAME="SO-Files"
+# Set the workflow name
+WORKFLOW_NAME="test-ssla-certs"
 
 
 
-# Set the GitHub API URL for artifacts
-API_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runs/${WORKFLOW_RUN_ID}/artifacts"
+# Set the GitHub API URL for workflow runs
+API_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/workflows/${WORKFLOW_NAME}/runs"
 
-# Use curl to retrieve artifact information
+# Use curl to retrieve workflow runs
 response=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" $API_URL)
 
-# Extract artifact ID based on the provided artifact name
-artifact_id=$(echo "$response" | jq -r ".artifacts[] | select(.name == \"$ARTIFACT_NAME\") | .id")
+# Extract workflow run IDs using jq
+workflow_run_ids=$(echo "$response" | jq -r '.workflow_runs[].id')
 
-if [ -z "$artifact_id" ]; then
-  echo "Artifact with name '$ARTIFACT_NAME' not found."
-  exit 1
-fi
-
-# Download the entire artifact as a zip file
-ARTIFACT_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/artifacts/$artifact_id/zip"
-curl -L -o "artifact_$artifact_id.zip" -H "Authorization: Bearer $GITHUB_TOKEN" $ARTIFACT_URL
-
-# Unzip the downloaded artifact
-unzip "artifact_$artifact_id.zip" -d "extracted_artifact_$artifact_id"
-
-# Find all CSV files in the extracted directory
-csv_files=$(find "extracted_artifact_$artifact_id" -type f -name '*.csv')
-
-if [ -z "$csv_files" ]; then
-  echo "No CSV files found in the artifact."
-  exit 1
-fi
-
-# Move or process each CSV file as needed
-for csv_file in $csv_files; do
-  mv "$csv_file" "desired-destination-directory"
-done
-
-# Clean up: remove downloaded zip file and extracted directory
-rm "artifact_$artifact_id.zip"
-rm -rf "extracted_artifact_$artifact_id"
-
+# Print the workflow run IDs
+echo "Workflow Run IDs:"
+echo "$workflow_run_ids"
