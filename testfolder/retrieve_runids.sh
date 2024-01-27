a3956d4e5494e8cad96f5c6558f77d7070c20393
upendraddev <81978371+upendraddev@users.noreply.github.com>
2024-01-27 20:22:21 +0530

    test

--- testfolder/retrieve_runids.sh
+++ testfolder/retrieve_runids.sh
@@ -4,21 +4,42 @@
 REPO_OWNER="upendraddev"
 REPO_NAME="azuredev"
 
-# Set the workflow name
-WORKFLOW_NAME="test-ssla-certs"
+# Set the workflow run ID for which you want to retrieve artifacts
+WORKFLOW_RUN_ID="7677580437"
+
+# Set the name of the artifact you want to download
+ARTIFACT_NAME="test-ssla-certs"
+
+# Set the path to the specific file within the artifact
+SPECIFIC_FILE_PATH="./opsjob/*.csv"
 
 # Set the Personal Access Token (PAT)
+#GITHUB_TOKEN="your-personal-access-token"
 
+# Set the GitHub API URL for artifacts
+API_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runs/${WORKFLOW_RUN_ID}/artifacts"
 
-# Set the GitHub API URL for workflow runs
-API_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/workflows/${WORKFLOW_NAME}/runs"
-
-# Use curl to retrieve workflow runs
+# Use curl to retrieve artifact information
 response=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" $API_URL)
 
-# Extract workflow run IDs using jq (make sure jq is installed)
-workflow_run_ids=$(echo "$response" | jq -r '.workflow_runs[].id')
+# Extract artifact ID based on the provided artifact name
+artifact_id=$(echo "$response" | jq -r ".artifacts[] | select(.name == \"$ARTIFACT_NAME\") | .id")
 
-# Print the workflow run IDs
-echo "Workflow Run IDs:"
-echo "$workflow_run_ids"
+if [ -z "$artifact_id" ]; then
+  echo "Artifact with name '$ARTIFACT_NAME' not found."
+  exit 1
+fi
+
+# Download the entire artifact as a zip file
+ARTIFACT_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/artifacts/$artifact_id/zip"
+curl -L -o "artifact_$artifact_id.zip" -H "Authorization: Bearer $GITHUB_TOKEN" $ARTIFACT_URL
+
+# Unzip the downloaded artifact
+unzip "artifact_$artifact_id.zip" -d "extracted_artifact_$artifact_id"
+
+# Move or process the specific file as needed
+mv "extracted_artifact_$artifact_id/$SPECIFIC_FILE_PATH" "desired-destination-directory"
+
+# Clean up: remove downloaded zip file and extracted directory
+rm "artifact_$artifact_id.zip"
+rm -rf "extracted_artifact_$artifact_id"
