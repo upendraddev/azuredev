db3215670b734484c0245d438edf9252a77339b0
upendraddev <81978371+upendraddev@users.noreply.github.com>
2024-01-29 13:47:21 +0530

    test

--- testfolder/retrieve_artifacts_csv.sh
+++ testfolder/retrieve_artifacts_csv.sh
@@ -9,44 +9,45 @@ FILE_TYPE=".csv"  # Specify the file type you are looking for
 
 # Get all workflow run IDs for the specified workflow
 # Set the GitHub API URL for workflow runs
-API_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runs?workflow=${WORKFLOW_NAME}"
+API_URL="https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/workflows/$WORKFLOW_NAME/runs"
 
 # Use curl to retrieve workflow runs
 response=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" $API_URL)
-# echo "$response"
+echo "$response"
 
 # Extract run IDs using jq
 RUN_IDS=$(echo "$response" | jq -r '.workflow_runs[].id')
 
+echo "$RUN_IDS"
 # RUN_IDS ="7677620182,7691725319,7677580437"
+# 
+# for RUN_ID in "${RUN_IDS}"; do
+#     #echo "$RUN_ID"
+#   # Get artifact information for a specific run
+#   ARTIFACT_INFO=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
+#     "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/runs/$RUN_ID/artifacts")
 
-for RUN_ID in "${RUN_IDS}"; do
-    #echo "$RUN_ID"
-  # Get artifact information for a specific run
-  ARTIFACT_INFO=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
-    "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/runs/$RUN_ID/artifacts")
+#   # Extract artifact ID and name from the response
+#   ARTIFACT_ID=$(echo "$ARTIFACT_INFO" | jq -r '.artifacts[0].id')
+#   ARTIFACT_NAME=$(echo "$ARTIFACT_INFO" | jq -r '.artifacts[0].name')
 
-  # Extract artifact ID and name from the response
-  ARTIFACT_ID=$(echo "$ARTIFACT_INFO" | jq -r '.artifacts[0].id')
-  ARTIFACT_NAME=$(echo "$ARTIFACT_INFO" | jq -r '.artifacts[0].name')
-
-  if [ "$ARTIFACT_ID" != "null" ] && [ "$ARTIFACT_NAME" != "null" ]; then
-      echo "Artifact ID: $ARTIFACT_ID"
-      echo "Artifact Name: $ARTIFACT_NAME"
-  fi
-    # Download the artifact as a zip file
-    # curl -L -o "artifact.zip" -H "Authorization: Bearer $GITHUB_TOKEN" \
-    # "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/artifacts/$ARTIFACT_ID/zip"
-    # sudo apt-get install unzip
+#   if [ "$ARTIFACT_ID" != "null" ] && [ "$ARTIFACT_NAME" != "null" ]; then
+#       echo "Artifact ID: $ARTIFACT_ID"
+#       echo "Artifact Name: $ARTIFACT_NAME"
+#   fi
+#     # Download the artifact as a zip file
+#     # curl -L -o "artifact.zip" -H "Authorization: Bearer $GITHUB_TOKEN" \
+#     # "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/artifacts/$ARTIFACT_ID/zip"
+#     # sudo apt-get install unzip
     
-    # Unzip the downloaded artifact
-    # unzip -j "artifact.zip" "*$FILE_TYPE"  -d "extracted_artifact_$artifact"
+#     # Unzip the downloaded artifact
+#     # unzip -j "artifact.zip" "*$FILE_TYPE"  -d "extracted_artifact_$artifact"
 
-    # mv "extracted_artifact_$artifact" "./testssl"
-    # Move or process the specific file as needed
+#     # mv "extracted_artifact_$artifact" "./testssl"
+#     # Move or process the specific file as needed
 
 
-    # Optionally, remove the downloaded zip file
-    # rm "artifact.zip"
-done
+#     # Optionally, remove the downloaded zip file
+#     # rm "artifact.zip"
+# done
 
