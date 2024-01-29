12fb856cf485c838347735500b24cafe128bf778
upendraddev <81978371+upendraddev@users.noreply.github.com>
2024-01-29 11:04:36 +0530

    tets

--- testfolder/retrieve_artifacts_csv.sh
+++ testfolder/retrieve_artifacts_csv.sh
@@ -18,14 +18,19 @@ response=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" $API_URL)
 RUN_IDS=$(echo "$response" | jq -r '.workflow_runs[].id')
 
 for RUN_ID in "${RUN_IDS}"; do
-    echo "$RUN_ID"
-    ARTIFACT_INFO=$(curl -s -H "Authorization: Bearer $TOKEN" \
-    "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/runs/$RUN_ID/artifacts")
+    #echo "$RUN_ID"
+    # Set the GitHub API URL for artifacts
+    API_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runs/${RUN_ID}/artifacts"
 
-    echo "$ARTIFACT_INFO"
-    # Extract artifact ID from the response
-    #ARTIFACT_ID=$(echo "$ARTIFACT_INFO" | jq -r '.artifacts[0].id')
+    # Use curl to retrieve artifact information
+    response=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" $API_URL)
 
-    #echo "Artifact ID for Run $RUN_ID: $ARTIFACT_ID"
+    # Extract artifact ID based on the provided artifact name
+    artifact_id=$(echo "$response" | jq -r ".artifacts[] | select(.name == \"$ARTIFACT_NAME\") | .id")
+
+    if [ -z "$artifact_id" ]; then
+      echo "Artifact with name '$ARTIFACT_NAME' not found."
+      exit 1
+    fi
 done
 
