305bebf5905de818cab5957201299af699c5c530
upendraddev <81978371+upendraddev@users.noreply.github.com>
2024-01-29 09:54:14 +0530

    test

--- testfolder/retrieve_artifacts_csv.sh
+++ testfolder/retrieve_artifacts_csv.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+REPO_OWNER="upendraddev"
+REPO_NAME="azuredev"
 WORKFLOW_NAME="test-ssla-certs"
 ARTIFACT_NAME="SO-Files"
 FILE_TYPE=".csv"  # Specify the file type you are looking for
 
 # Get all workflow run IDs for the specified workflow
 RUN_IDS=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
-  "https://api.github.com/repos/{owner}/{repo}/actions/workflows/$WORKFLOW_NAME/runs" \
+  "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/workflows/runs?workflow=${WORKFLOW_NAME}" \
   | jq -r '.workflow_runs[].id')
 
 # Loop through each run ID and download the artifact if it contains a CSV file
 for RUN_ID in $RUN_IDS; do
   # Get the download URL for the artifact
   DOWNLOAD_URL=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
-    "https://api.github.com/repos/{owner}/{repo}/actions/runs/$RUN_ID/artifacts/$ARTIFACT_NAME" \
+    "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/runs/$RUN_ID/artifacts/$ARTIFACT_NAME" \
     | jq -r '.archive_download_url')
 
   # Download the artifact as a zip file
