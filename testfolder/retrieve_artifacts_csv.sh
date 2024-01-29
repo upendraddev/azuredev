9d2062787bfaaa9e5f3425c68b4b1e9bd829960a
upendraddev <81978371+upendraddev@users.noreply.github.com>
2024-01-29 11:13:30 +0530

    test

--- testfolder/retrieve_artifacts_csv.sh
+++ testfolder/retrieve_artifacts_csv.sh
@@ -18,13 +18,14 @@ response=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" $API_URL)
 RUN_IDS=$(echo "$response" | jq -r '.workflow_runs[].id')
 
 for RUN_ID in "${RUN_IDS}"; do
-    #echo "$RUN_ID"
-    ARTIFACT_INFO=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
-     "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/runs/$RUN_ID/artifacts")
+    DOWNLOAD_URL=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
+    "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/runs/$RUN_ID/artifacts/$ARTIFACT_NAME" \
+    | jq -r '.archive_download_url')
+    echo "$DOWNLOAD_URL"
+    # Download the artifact as a zip file
+    curl -L -o "artifact.zip" "$DOWNLOAD_URL"
 
-    # Extract artifact ID from the response
-    ARTIFACT_ID=$(echo "$ARTIFACT_INFO" | jq -r '.artifacts[0].id')
-
-    echo "Artifact ID for Run $RUN_ID: $ARTIFACT_ID"
+    # Extract CSV files from the zip archive and store in a directory
+    unzip -j "artifact.zip" "*$FILE_TYPE" -d "./$RUN_ID"
 done
 
