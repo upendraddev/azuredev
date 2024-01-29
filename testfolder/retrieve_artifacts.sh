56fe89a7ec1c2b0733bafdc6e66d453f556d2de6
upendraddev <81978371+upendraddev@users.noreply.github.com>
2024-01-29 11:36:54 +0530

    test

--- testfolder/retrieve_artifacts.sh
+++ testfolder/retrieve_artifacts.sh
@@ -5,7 +5,7 @@ REPO_OWNER="upendraddev"
 REPO_NAME="azuredev"
 RUN_ID="7677620182"
 ARTIFACT_NAME="SO-Files"
-CSV_FILE_NAME="20240127090347-SO_Outputs.csv"
+CSV_FILE_NAME="./20240127090347-SO_Outputs.csv"
 
 # Get artifact information for a specific run
 ARTIFACT_INFO=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" \
@@ -22,8 +22,19 @@ echo "Artifact Name: $ARTIFACT_NAME"
 curl -L -o "artifact.zip" -H "Authorization: Bearer $GITHUB_TOKEN" \
   "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/artifacts/$ARTIFACT_ID/zip"
 
+
+
 # Extract the CSV file from the zip archive
-unzip -j "artifact.zip" "$ARTIFACT_NAME/$CSV_FILE_NAME" -d "./"
+#unzip -j "artifact.zip" "$ARTIFACT_NAME/$CSV_FILE_NAME" -d "./"
+
+
+# Unzip the downloaded artifact
+unzip "$artifact.zip" -d "extracted_artifact_$artifact"
+
+# Move or process the specific file as needed
+mv "extracted_artifact_$artifact_id/$CSV_FILE_NAME" "./utils"
+
+
 
 # Optionally, remove the downloaded zip file
 rm "artifact.zip"
