932a9e4a874f56272a46289033363bac64605e48
upendraddev <81978371+upendraddev@users.noreply.github.com>
2024-01-30 12:35:09 +0530

    test

--- opsjob/scripts/read-csv-data.py
+++ opsjob/scripts/read-csv-data.py
@@ -10,7 +10,7 @@ for i in files:
         filename=i
 chipIDs=sys.argv[1]
 csv_data=[]
-if filename != "":
+if filename == "":
     filename=datetime.datetime.now().strftime("%Y%m%d%H%M%S")+"SO_Outputs.csv"
     csv_data=[["S.No","ChipID","Date","RequestedCount"]]
     count=1
@@ -22,14 +22,14 @@ else:
         csvFile = csv.reader(file)
         for lines in csvFile:  
             csv_data.append(lines)
-    print(csv_data)
+    # print(csv_data)
     for i in range(len(csv_data)):
         for j in chipIDs.split(","):
             if j in csv_data[i]:
                 csv_data[i][2]=datetime.datetime.now().strftime("%Y-%m-%d")
                 csv_data[i][3]=int(csv_data[i][3])+1
  
-print(csv_data)
+# print(csv_data)
 with open(filename, 'w') as csvfile:  
     csvwriter = csv.writer(csvfile)  
     csvwriter.writerows(csv_data)
