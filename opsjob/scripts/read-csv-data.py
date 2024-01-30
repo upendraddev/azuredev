a459453b9741ecb6592052c1a69e1600f7137697
upendraddev <81978371+upendraddev@users.noreply.github.com>
2024-01-30 12:48:23 +0530

    test

--- opsjob/scripts/read-csv-data.py
+++ opsjob/scripts/read-csv-data.py
@@ -8,27 +8,30 @@ filename=""
 for i in files:
     if ".csv" in i:
         filename=i
-chipIDs=sys.argv[1]
+chipIDs=sys.argv[1].split(",")
 csv_data=[]
 if filename == "":
-    filename=datetime.datetime.now().strftime("%Y%m%d%H%M%S")+"SO_Outputs.csv"
-    csv_data=[["S.No","ChipID","Date","RequestedCount"]]
+    filename=datetime.datetime.now().strftime("%Y%m%d%H%M%S")+"-SoFiles.csv"
+    csv_data=[["s.no","ChipID","Date","RequestedCount"]]
     count=1
-    for j in chipIDs.split(","):
+    for j in chipIDs:
         csv_value=[count,j,datetime.datetime.now().strftime("%Y-%m-%d"),1]
         csv_data.append(csv_value)
+        count+=1
 else:
     with open(filename, mode ='r')as file:  
         csvFile = csv.reader(file)
         for lines in csvFile:  
             csv_data.append(lines)
-    # print(csv_data)
-    for i in range(len(csv_data)):
-        for j in chipIDs.split(","):
+    for j in chipIDs:
+        chipexist=False
+        for i in range(len(csv_data)):
             if j in csv_data[i]:
+                chipexist=True
                 csv_data[i][2]=datetime.datetime.now().strftime("%Y-%m-%d")
-                csv_data[i][3]=int(csv_data[i][3])+1
- 
+                csv_data[i][3]=int(csv_data[i][3])+1            
+        if not chipexist:
+            csv_data.append([len(csv_data),j,datetime.datetime.now().strftime("%Y-%m-%d"),1])
 # print(csv_data)
 with open(filename, 'w') as csvfile:  
     csvwriter = csv.writer(csvfile)  
