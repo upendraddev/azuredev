e7eee7dbd497f98d42e5f7e372063a8a55496083
upendraddev <81978371+upendraddev@users.noreply.github.com>
2024-01-30 11:20:34 +0530

    test

---
+++ opsjob/scripts/read-csv-data.py
@@ -0,0 +1,35 @@
+import csv  
+import sys
+import datetime
+import os
+ 
+files = os.listdir()
+filename=""
+for i in files:
+    if ".csv" in i:
+        filename=i
+chipIDs=sys.argv[1]
+csv_data=[]
+if filename == "":
+    filename=datetime.datetime.now().strftime("%Y%m%d%H%M%S")+"SO_Outputs.csv"
+    csv_data=[["S.No","ChipID","Date","RequestedCount"]]
+    count=1
+    for j in chipIDs.split(","):
+        csv_value=[count,j,datetime.datetime.now().strftime("%Y-%m-%d"),1]
+        csv_data.append(csv_value)
+else:
+    with open(filename, mode ='r')as file:  
+        csvFile = csv.reader(file)
+        for lines in csvFile:  
+            csv_data.append(lines)
+ 
+    for i in range(len(csv_data)):
+        for j in chipIDs.split(","):
+            if j in csv_data[i]:
+                csv_data[i][2]=datetime.datetime.now().strftime("%Y-%m-%d")
+                csv_data[i][3]=int(csv_data[i][3])+1
+ 
+print(csv_data)
+with open(filename, 'w') as csvfile:  
+    csvwriter = csv.writer(csvfile)  
+    csvwriter.writerows(csv_data)
---
+++ opsjob/scripts/run_sssla_cetificate_operations_latest.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+echo -e "\n===== [$(readlink -f $0)] =====\n" 1>&2
+set -euvx
+shopt -s inherit_errexit
+set +v
+
+: $1 $2 $3 $4
+
+
+
+
+url=$1
+ChipID=$2
+token=$3
+dir=$4
+csvfilname=$5
+ChipIDC=$6
+
+
+
+serial_number="vferuwefl3743y93192"
+if [[ "$serial_number" == "null" ]]; then
+echo "No Records Found related to $ChipID.Please provide valid ChipID!"
+exit 1
+else
+certificate_pem="eftgfdaf356r4i2tdmgbgu35yl475934fbdsjhvfdfsdgsckafldjsgbbsd"
+echo $certificate_pem > ./opsjob/"$dir"/"$ChipID"_SO_Output.pem
+sudo chmod 777 ./opsjob/"$csvfilname"
+echo "$ChipID,1" >> ./opsjob/"$csvfilname"
+fi
