efeaac9117cd23932edbabed3f5bd7de96abf6fb
upendraddev <81978371+upendraddev@users.noreply.github.com>
2024-10-04 15:40:04 +0530

    Update test.sh

--- testfolder/test.sh
+++ testfolder/test.sh
@@ -5,11 +5,13 @@ set -euvx -o pipefail
 shopt -s inherit_errexit
 
 
-Env="tessghc2456"
-TitleMessage="teysav4556"
-DDAPPKEY="svjgcjmd3564"
-ACIC="2345678"
-GSAQ="67896767"
+# Env="tessghc2456"
+# TitleMessage="teysav4556"
+# DDAPPKEY="svjgcjmd3564"
+# ACIC="2345678"
+# GSAQ="67896767"
 
-echo "acic=${ACIC}" >> $GITHUB_OUTPUT
-echo "gsaq=${GSAQ}" >> $GITHUB_OUTPUT
+# echo "acic=${ACIC}" >> $GITHUB_OUTPUT
+# echo "gsaq=${GSAQ}" >> $GITHUB_OUTPUT
+FILE_CONTENT=$(cat samplefile.txt)
+echo "FILE_CONTENT=${FILE_CONTENT}" >> $GITHUB_ENV
